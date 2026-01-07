8690a7e09f41b2f9295ebbe55d5e39256c97325f
jchuang <jchuang@mitac.com.tw>
2026-01-07 11:38:42 +0800

    20260107 2st add app

--- main.py
+++ main.py
@@ -8,5 +8,9 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+@app.get("/test")
+async def test():
+    return {"message": "It works well!"}
+
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
