9bf31435abbfd4d3382b7816bbd1f1bbe2f6d429
jchuang <jchuang@mitac.com.tw>
2026-03-16 14:56:03 +0800

    20260316-1455 remove route

--- main.py
+++ main.py
@@ -18,10 +18,6 @@ async def test():
 async def get_key():
     return {"message": os.getenv("test_key1")}
 
-@app.get("/get_key_2")
-async def get_key():
-    return {"message": os.getenv("test_key2")}
-
 @app.get("/move_file")
 async def move_file():
     src_bucket_name='jchuang_no_hierarchical'
